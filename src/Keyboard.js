7f806ae7331c27eddb7bb4e31636bc54d5bdf0d2
Logan Arnett <logan@Logans-MacBook-Pro.local>
2017-03-10 12:56:47 -0500

    removing space hook

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -204,7 +204,7 @@ export default class Keyboard extends PureComponent {
 					: null}
 					<KeyboardButton
 						value={space}
-						classes={`keyboard-space ${space === 'hidden' ? 'hide' : ''}`}
+						classes="keyboard-space"
 						onClick={this.handleLetterButtonClick}
 					/>
 					{inputNode.dataset.type === 'email' ?
